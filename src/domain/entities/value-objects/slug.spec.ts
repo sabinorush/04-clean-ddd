3b09f0af0f1df3e88ffec7e6f677f333f5488d95
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-20 15:25:35 -0300

    initial commid

---
+++ src/domain/entities/value-objects/slug.spec.ts
@@ -0,0 +1,7 @@
+import { Slug } from "./slug";
+
+it('should be able to create a new slug from test', () => {
+  const slug = Slug.createFromText('Example question title')
+
+  expect(slug.value).toBe('example-question-title')
+})
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,21 @@
+import { AnswerQuestionUseCase } from './answer-question'
+import { Answersrepository } from '../../repositories/answers-repository'
+import { Answer } from '../entities/answer'
+
+const fakeAnswersRepository: Answersrepository = {
+  create: async (answer: Answer) => {
+    return;
+  }
+}
+
+it('should be able to create an answer', async () => {
+  const AnswerQuestion = new AnswerQuestionUseCase(fakeAnswersRepository)
+
+  const answer = await AnswerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova resposta',
+  })
+
+  expect(answer.content).toEqual('Nova resposta')
+})
