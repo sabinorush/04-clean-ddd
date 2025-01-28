b95cdca5b04959a8fa229a663dde439c96077539
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-28 17:03:09 -0300

    chore: adding use-case for editing a question

---
+++ src/domain/forum/application/use-cases/edit-question.ts
@@ -0,0 +1,38 @@
+import { QuestionsRepository } from '../repositories/question-repository';
+
+interface EditQuestionUseCaseRequest {
+  authorId: string;
+  questionId: string;
+  title: string;
+  content: string;
+}
+
+// eslint-disable-next-line
+interface EditQuestionUseCaseResponse {}
+
+export class EditQuestionUseCase {
+  constructor(private questionRepository: QuestionsRepository) {}
+  async execute({
+    authorId,
+    questionId,
+    title,
+    content,
+  }: EditQuestionUseCaseRequest): Promise<EditQuestionUseCaseResponse> {
+    const question = await this.questionRepository.findById(questionId);
+
+    if (!question) {
+      throw new Error('Question not found.');
+    }
+
+    if (authorId !== question.authorId.toString()) {
+      throw new Error('Not Allowed.');
+    }
+
+    question.title = title;
+    question.content = content;
+
+    await this.questionRepository.save(question);
+
+    return {};
+  }
+}
