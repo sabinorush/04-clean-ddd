eb89e6e188e0736fe3dda27a2c58def22b007b9b
Gustavo Sabino <gu.sabino@hotmail.com>
2025-01-30 21:37:42 -0300

    chore: adding use-case to list question answers

---
+++ src/domain/forum/application/use-cases/fetch-question-answer.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository';
+import { FetchQuestionsAnswersUseCase } from './fetch-question-answers';
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+import { makeAnswer } from 'test/factories/make-answer';
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository;
+let sut: FetchQuestionsAnswersUseCase;
+// system under test
+
+describe('Fetch Question Answers', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository();
+    sut = new FetchQuestionsAnswersUseCase(inMemoryAnswersRepository);
+  });
+
+  it('should be able to fetch question answers', async () => {
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    );
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    );
+    await inMemoryAnswersRepository.create(
+      makeAnswer({
+        questionId: new UniqueEntityID('question-1'),
+      })
+    );
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    });
+
+    expect(answers).toHaveLength(3);
+  });
+
+  it('should be able to fetch paginated answers', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryAnswersRepository.create(
+        makeAnswer({
+          questionId: new UniqueEntityID('question-1'),
+        })
+      );
+    }
+
+    const { answers } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    });
+
+    expect(answers).toHaveLength(2);
+  });
+});
