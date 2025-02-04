3ee4bac9f6102ec01a1dd90bb5d12ec2a08942d4
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-04 14:09:41 -0300

    chore: adding use case for comment on a question

--- src/domain/forum/application/use-cases/choose-question-best-answer.ts
+++ src/domain/forum/application/use-cases/choose-question-best-answer.ts
@@ -1,5 +1,3 @@
-import { UniqueEntityID } from '@/core/entities/unique-entity-id';
-import { Answer } from '../../enterprise/entities/answer';
 import { AnswersRepository } from '../repositories/answers-repository';
 import { Question } from '../../enterprise/entities/question';
 import { QuestionsRepository } from '../repositories/question-repository';
