43b48f7b4e51e59255c5d57ee30973f2224b4f5a
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-04 15:14:41 -0300

    chore: adding use case for delete a answer comment

---
+++ test/factories/make-answer-comment.ts
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker';
+
+import { UniqueEntityID } from '@/core/entities/unique-entity-id';
+
+import {
+  AnswerComment,
+  AnswerCommentProps,
+} from '@/domain/forum/enterprise/entities/answer-comment';
+
+export function makeAnswerComment(
+  override: Partial<AnswerCommentProps> = {},
+  id?: UniqueEntityID
+) {
+  const answerComment = AnswerComment.create(
+    {
+      authorId: new UniqueEntityID(),
+      answerId: new UniqueEntityID(),
+      content: faker.lorem.sentence(),
+      ...override,
+    },
+    id
+  );
+
+  return answerComment;
+}
