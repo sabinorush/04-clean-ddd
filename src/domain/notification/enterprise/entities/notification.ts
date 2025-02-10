df54bdc9055fa560bd559fd1ee925d712bb41684
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-10 20:36:13 -0300

    chore: Subdomain & Domain Events - Use Case for read a notification

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -2,7 +2,7 @@ import { Optional } from '@/core/@types/optinional';
 import { Entity } from '@/core/entities/entity';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 
-interface NotificationProps {
+export interface NotificationProps {
   recipientId: UniqueEntityID;
   title: string;
   content: string;
@@ -28,6 +28,10 @@ export class Notification extends Entity<NotificationProps> {
 
   get createdAt() {
     return this.props.createdAt;
+  }
+
+  read() {
+    this.props.readAt = new Date();
   }
 
   static create(
